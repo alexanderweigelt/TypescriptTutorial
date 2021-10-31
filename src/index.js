18f7aa2cfa53240d8f088ca65f799fd29f2271b8
Alexander Weigelt <webdesign@alexander-weigelt.de>
2021-10-31 11:22:43 +0100

    Chapter I - TypeScript Tutorial - Deploy an interface - Debug the TypeScript code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/normalize.css';
 import './css/main.css';
 
-import './js/plugins';
+//import './js/plugins';
 import './js/main';
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,3 +1,50 @@
 /**
  * TypeScript tutorial code
  */
+
+/**
+ * Set all friends data
+ */
+const friends = [
+    {firstName: 'Alex', age: 47},
+    {firstName: 'Lillie'},
+    {firstName: 'Mia'},
+    {firstName: 'Konstanze'}
+];
+
+interface Friends {
+    firstName: string;
+    age?: number;
+}
+
+/**
+ * Prints friends firstname
+ * @param {*} friends an array of friends
+ */
+function printFirstName(friends: Friends[]): void {
+    for (const friend of friends) {
+        hello(friend.firstName);
+        if (friend.age) {
+            age(friend.age, friend.firstName);
+        }
+    }
+}
+
+/**
+ * Says hello `name`
+ * @param name
+ */
+function hello(name: string): void {
+    console.log('Hello ' + name + '!!!');
+}
+
+/**
+ * Gets the friends age
+ * @param age
+ * @param name
+ */
+function age(age: number, name: string): void {
+    console.log(name + ' is ' + age + ' years old');
+}
+
+printFirstName(friends);
